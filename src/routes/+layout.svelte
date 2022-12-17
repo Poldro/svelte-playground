545f6133b6fc858dc7ebb08f337758c1b4c44c3a
Poldro <valmori.paolo@gmail.com>
2022-12-17 04:09:55 +0100

    page to call open ai

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import './styles.css';
 </script>
 
+<slot />
 <div class="app">
 	<Header />
 
@@ -11,43 +12,8 @@
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<p class="text-3xl font-bold underline">
+			visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit
+		</p>
 	</footer>
 </div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>
