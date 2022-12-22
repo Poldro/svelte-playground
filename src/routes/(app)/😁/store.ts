b5c0c45dd1a2a91074905b095880248891e3e92f
poldro <paolo.valmori@gmail.com>
2022-12-22 02:12:04 +0100

    moved answer format to parent

--- src/routes/(app)/😁/store.ts
+++ src/routes/(app)/😁/store.ts
@@ -2,4 +2,4 @@ import { writable } from 'svelte/store';
 import type { ActionData } from './$types';
 
 export const inputTextChat = writable<string>('');
-export const chatData= writable<{question:string,answer:string,type:string}[] >([]);
+export const chatData= writable<{question:string,answer:string[],type:string}[] >([]);
